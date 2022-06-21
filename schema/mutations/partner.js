f7fa51f4ffba0ac084fd805d9c2b8fc34de47d27
adibradu <adrian.bradu@gmail.com>
2022-06-21 13:30:50 +0300

    delete partner correction

--- schema/mutations/partner.js
+++ schema/mutations/partner.js
@@ -103,15 +103,17 @@ exports.DELETE_PARTNER = {
     }
     let successful = false;
     let message = `Eroare! Terapeutul inclus in sedinte programate in viitor`;
-    let currentDateTime = new Date().now();
+    let currentDateTime = Date.now();
     let sqlHasPendingAppointments = `SELECT od_id 
                                       FROM order_details 
                                       WHERE partner_id = ? AND appointment_end > ?`;
-    let resHasPendingAppointments = await conn.promise().query(sqlHasPendingAppointments, [args.id, currentDateTime]);
-    if (!resHasPendingAppointments[0].length) {     
+    let resHasPendingAppointments = await conn
+      .promise()
+      .query(sqlHasPendingAppointments, [args.id, currentDateTime]);
+    if (!resHasPendingAppointments[0].length) {
       await conn
         .promise()
-        .query(`UPDATE partners SET is_deleted = 1 WHERE p_id = ?`, [args.id]);                             
+        .query(`UPDATE partners SET is_deleted = 1 WHERE p_id = ?`, [args.id]);
       await conn
         .promise()
         .query(`DELETE FROM partner_schedule WHERE partner_id = ?`, [args.id]);
